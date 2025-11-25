580a61dde0b69585fdaaee3a6790abaf1bc5d0f1
Riakol <inspi47@gmail.com>
2025-11-25 19:45:19 +0500

    feat: Add custom theme colors
    Added a new set of custom colors to Color.kt for a defined application theme, including colors for background, surface, primary actions, text, and status indicators.

--- app/src/main/java/com/riakol/rollcall/MainActivity.kt
+++ app/src/main/java/com/riakol/rollcall/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.riakol.rollcall.ui.theme.RollCallTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
